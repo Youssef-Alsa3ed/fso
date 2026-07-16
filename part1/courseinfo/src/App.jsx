0acb255a731a4bcc325ae8b0894457f9bd70214e
youssef sayed <80265509+Youssef-Alsa3ed@users.noreply.github.com>
2026-07-16 05:00:06 +0300

    exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,26 +1,28 @@
 import { useState } from "react";
 
-const Header = () => {
-  const course = "Half Stack application development";
+const Header = ({ course }) => {
   return <h1>{course}</h1>;
 };
 
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
 const Content = ({ parts }) => {
   return (
     <>
       {parts.map((part, index) => {
-        return (
-          <p key={index}>
-            {part.name} {part.exercise}
-          </p>
-        );
+        return <Part key={index} part={part} />;
       })}
     </>
   );
 };
 
 const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercise, 0);
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
 
   return (
     <>
@@ -30,14 +32,16 @@ const Total = ({ parts }) => {
 };
 
 const App = () => {
+  const course = "Half Stack application development";
+
   const parts = [
-    { name: "Fundamentals of React", exercise: 10 },
-    { name: "Using props to pass data", exercise: 7 },
-    { name: "State of a component", exercise: 14 },
+    { name: "Fundamentals of React", exercises: 10 },
+    { name: "Using props to pass data", exercises: 7 },
+    { name: "State of a component", exercises: 14 },
   ];
   return (
     <div>
-      <Header />
+      <Header course={course} />
       <Content parts={parts} />
       <Total parts={parts} />
     </div>
