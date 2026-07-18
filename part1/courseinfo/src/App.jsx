7730f8bc99e9f31170f46f22c2f43a7382decb63
youssef sayed <80265509+Youssef-Alsa3ed@users.noreply.github.com>
2026-07-18 06:20:55 +0300

    1.8: unicafe step 3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
-const App = () => {
-  const [good, setGood] = useState(0);
-  const [bad, setBad] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-
+const Statistics = ({ good, bad, neutral }) => {
   const total = good + bad + neutral;
   const average = (good - bad) / total;
   const positive = (good / total) * 100.0;
+
+  if (total == 0) {
+    return (
+      <>
+        <h1>Statistics</h1>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+      </>
+    );
+  }
+
   return (
-    <div>
-      <h1>give feedback</h1>
-      <Button onClick={() => setGood(good + 1)} text="good" />
-      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button onClick={() => setBad(bad + 1)} text="bad" />
+    <>
       <h1>Statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
@@ -22,6 +25,22 @@ const App = () => {
       <p>total {total}</p>
       <p>average {average}</p>
       <p>positive {positive}</p>
+    </>
+  );
+};
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   );
 };
