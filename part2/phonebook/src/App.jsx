df0c9419dae5345013984983efa3953ea119a6c3
youssef sayed <80265509+Youssef-Alsa3ed@users.noreply.github.com>
2026-07-20 01:34:19 +0300

    2.11: The Phonebook Step 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function isEmpty(str) {
   return !str || str.length === 0;
@@ -95,14 +96,17 @@ const PersonList = ({ persons, search }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [search, setSearch] = useState("");
 
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise succeeded.");
+      setPersons(response.data);
+    });
+  }, []);
+  console.log("render");
   return (
     <div>
       <h2>Phonebook</h2>
