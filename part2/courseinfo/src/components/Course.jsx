e315ddd715c7ed0e371b5df28bb3440225b103b8
youssef sayed <80265509+Youssef-Alsa3ed@users.noreply.github.com>
2026-07-19 04:35:13 +0300

    2.2 + 2.3

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -34,28 +34,9 @@ const Course = ({ course }) => {
     <>
       <Header course={course} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   );
 };
 
-//   const course = {
-//     id: 1,
-//     name: "Half Stack application development",
-//     parts: [
-//       {
-//         name: "Fundamentals of React",
-//         exercises: 10,
-//         id: 1,
-//       },
-//       {
-//         name: "Using props to pass data",
-//         exercises: 7,
-//         id: 2,
-//       },
-//       {
-//         name: "State of a component",
-//         exercises: 14,
-//         id: 3,
-//       },
-//     ]
 export default Course;
