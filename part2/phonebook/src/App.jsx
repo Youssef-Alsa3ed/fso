f8d88a8edfc13faf3dcb51a19e2ec7a2a7d831ae
youssef sayed <80265509+Youssef-Alsa3ed@users.noreply.github.com>
2026-07-20 05:45:48 +0300

    2.16: Phonebook step 11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,23 +3,35 @@ import { useEffect, useState } from "react";
 import { remove, getAll, create, update } from "./services/persons";
 
 import { Filter, PersonForm, PersonList } from "./components/persons";
-
+import { Notification } from "./components/notification";
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [search, setSearch] = useState("");
-
+  const [notification, setNotification] = useState(null);
+  const [notificationType, setNotificationType] = useState("notification");
   useEffect(() => {
     getAll().then((data) => {
       setPersons(data);
     });
   }, []);
 
+  const notify = (text, type = "green") => {
+    setNotificationType(type);
+    setNotification(text);
+
+    setTimeout(() => {
+      setNotification(null);
+    }, 3000);
+  };
   const handleDeletion = (id) => {
+    const deletedPerson = persons.find((person) => person.id === id);
     remove(id).then(() => {
       setPersons((currentPersons) =>
         currentPersons.filter((person) => person.id !== id),
       );
     });
+
+    notify(`${deletedPerson.name} has been deleted from the phonebook`, "red");
   };
 
   const handleCreation = (newPerson) => {
@@ -27,13 +39,14 @@ const App = () => {
       if (newPerson.name === person.name) {
         let isDuplicate = newPerson.number === person.number;
         if (isDuplicate) {
-          alert(`${person.name} is already added to phonebook`);
+          notify(`${person.name} is already added to phonebook`);
           return;
         } else {
           let id = person.id;
           update(id, newPerson).then((data) =>
             setPersons(persons.map((p) => (p.id === id ? data : p))),
           );
+          notify(`${person.name}'s number has been updated.`);
           return;
         }
       }
@@ -41,6 +54,7 @@ const App = () => {
     create(newPerson)
       .then((data) => {
         setPersons(persons.concat(data));
+        notify(`${data.name} has been added to the phonebook.`);
       })
       .catch((data) => {
         console.log("form submit failed!");
@@ -50,6 +64,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notification} type={notificationType} />
       <Filter search={search} setSearch={setSearch} />
       <h2>Add a new</h2>
       <PersonForm persons={persons} handleCreation={handleCreation} />
